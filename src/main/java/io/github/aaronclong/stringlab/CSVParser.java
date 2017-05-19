dacfbb0cfc6ae32b5369ca026930831186eed49c
Aaron Long <aaron.long.c@gmail.com>
2017-05-19 16:53:33 -0400

    Finished Heaeder Parser

--- src/main/java/io/github/aaronclong/stringlab/CSVParser.java
+++ src/main/java/io/github/aaronclong/stringlab/CSVParser.java
@@ -1,7 +1,22 @@
 package io.github.aaronclong.stringlab;
 
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by aaronlong on 5/19/17.
  */
 public class CSVParser {
+
+  public static ArrayList<String> getCSVHeader(String csv) {
+    Pattern pattern = Pattern.compile("([A-Z_\\.]+(?![a-z])[^,1-9])");
+    Matcher matcher = pattern.matcher(csv);
+    ArrayList<String> matchedColumns = new ArrayList<>();
+    while(matcher.find()) {
+      matchedColumns.add(matcher.group());
+    }
+    return matchedColumns;
+  }
+
 }
--- src/test/java/io/github/aaronclong/stringlab/CSVParserTest.java
+++ src/test/java/io/github/aaronclong/stringlab/CSVParserTest.java
@@ -1,6 +1,9 @@
 package io.github.aaronclong.stringlab;
 
 import org.junit.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -9,7 +12,13 @@ import static org.junit.Assert.assertEquals;
 public class CSVParserTest {
   @Test
   public void checkCSVHeader() {
-    String headerTest = "LAST_NAME,FIRST_NAME,ADDR,STATE,LICENSE_NUMBER,D.O.B.,ISS_D,EXP_D,SEX,EYES,HGT,ORGANDONOR,CLASS\n";
-    assertEquals("Checking that the strings are equal", headerTest, "");
+     String csvFirstLine = "LAST_NAME,FIRST_NAME,ADDR,STATE,LICENSE_NUMBER,D.O.B.,ISS_D,EXP_D,SEX,EYES,HGT,ORGANDONOR,CLASS";
+     String[] columns = new String[]{"LAST_NAME","FIRST_NAME","ADDR","STATE",
+             "LICENSE_NUMBER","D.O.B.","ISS_D","EXP_D",
+             "SEX","EYES","HGT","ORGANDONOR","CLASS"};
+    ArrayList<String> compareList = CSVParser.getCSVHeader(csvFirstLine);
+     for (int i = 0; i < compareList.size(); i++) {
+       assertEquals("Checking that the CSV Headers are equal", columns[i], compareList.get(i));
+     }
   }
 }
