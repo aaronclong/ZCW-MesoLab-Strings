ac96833a245ed469c3c7d32573e68bec425662d3
Aaron Long <aaron.long.c@gmail.com>
2017-05-20 20:47:14 -0400

    Started test for JSON parser

--- src/main/java/io/github/aaronclong/stringlab/JSONParser.java
+++ src/main/java/io/github/aaronclong/stringlab/JSONParser.java
@@ -4,5 +4,8 @@ package io.github.aaronclong.stringlab;
  * Created by aaronlong on 5/20/17.
  */
 public class JSONParser {
+  public static DriverLicense[] deserializeFromJSON(String json) {
+    return new DriverLicense[]{};
+  }
 
 }
--- src/test/java/io/github/aaronclong/stringlab/JSONParserTest.java
+++ src/test/java/io/github/aaronclong/stringlab/JSONParserTest.java
@@ -2,6 +2,7 @@ package io.github.aaronclong.stringlab;
 
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -12,11 +13,12 @@ import java.util.logging.Logger;
  * Created by aaronlong on 5/20/17.
  */
 public class JSONParserTest {
-  private static Logger logger = Logger.getLogger(CSVParserTest.class.toString());
+  private static Logger logger = Logger.getLogger(JSONParserTest.class.toString());
   private String jsonString;
+  private DriverLicense[] csvDriversTestAgainst;
 
   @Before
-  public void setUp() {
+  public void setUpForJSON() {
     try {
       ClassLoader classLoader = getClass().getClassLoader();
       Path file = Paths.get(classLoader.getResource("testJSONFile.json").toURI());
@@ -27,8 +29,27 @@ public class JSONParserTest {
     }
   }
 
+  @Before
+  public void setUpForCSVObject() {
+      try {
+        ClassLoader classLoader = getClass().getClassLoader();
+        Path file = Paths.get(classLoader.getResource("testCSVFile.csv").toURI());
+        byte[] csvBytes = Files.readAllBytes(file);
+        String csvString = new String(csvBytes);
+        csvDriversTestAgainst = CSVParser.deserializeFromCSV(csvString);
+      } catch(Exception e) {
+        logger.warning(e.toString());
+      }
+  }
+
   @Test
   public void testDeserializeJSON() {
-    logger.info(jsonString);
+    DriverLicense[] driversFromJSON = JSONParser.deserializeFromJSON(jsonString);
+    assertEquals("Checking for identical DriverLicense Object creation",
+            csvDriversTestAgainst.length, driversFromJSON.length);
+    for (int i = 0; i < driversFromJSON.length; i++) {
+      assertEquals("Checking for identical DriverLicense Object creation",
+              csvDriversTestAgainst[i], driversFromJSON[i];
+    }
   }
 }
