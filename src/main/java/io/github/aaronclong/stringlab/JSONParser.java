556d9abf141d271c8aa7aa05926f331c727d377c
Aaron Long <aaron.long.c@gmail.com>
2017-05-21 00:28:01 -0400

    Cleaned up the method && Regex

--- src/main/java/io/github/aaronclong/stringlab/JSONParser.java
+++ src/main/java/io/github/aaronclong/stringlab/JSONParser.java
@@ -11,19 +11,19 @@ public class JSONParser {
 
   private static String[] getKeyValuePair(String keyValue) {
     String[] result = new String[2];
-    Pattern pattern = Pattern.compile("[\\b\\w+'?.?\\/?]+");
+    Pattern pattern = Pattern.compile("(\\w+\\s?(\\.?)(\\/?)('?))+");
     Matcher match = pattern.matcher(keyValue);
-    ArrayList<String> list = new ArrayList<>();
-    while (match.find()) {
-      list.add(match.group());
+    for (int i = 0; i < 2; i++) {
+      if (!match.find()) {
+        return null;
+      }
+      result[i] = match.group();
     }
-    result[0] = list.get(0);
-    StringBuilder builder = new StringBuilder();
-    for (int i = 1; i < list.size(); i++) {
-      builder.append(list.get(i));
-    }
-    result[1] = builder.toString();
     return result;
+  }
+
+  private static void valueSetter(DriverLicense driver, String[] keyValue) {
+
   }
 
   private static DriverLicense stringToDriverLicense(String block) {
@@ -31,14 +31,16 @@ public class JSONParser {
     DriverLicense driver = DriverLicense.printLicense(new ArrayList<>());
     Matcher matcher = pattern.matcher(block);
     while (matcher.find()) {
-      String match = matcher.group();
-      
+      String[] match = getKeyValuePair(matcher.group());
+      if (match == null) {
+        return null;
+      }
+      valueSetter(driver, match);
     }
-
+    return driver;
   }
 
   public static DriverLicense[] deserializeFromJSON(String json) {
-
     return new DriverLicense[]{};
   }
 
